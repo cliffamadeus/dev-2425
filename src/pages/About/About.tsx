efd005a507c4d53d636fcc35d364414abdb679c9
cliffamadeus <dev.clfm5t12@gmail.com>
2025-01-10 14:27:00 +0800

    Initial About

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,28 +1,57 @@
 import { 
+  IonAvatar,
+  IonButton,
   IonButtons,
+    IonCol,
     IonContent, 
+    IonGrid, 
     IonHeader, 
+    IonInput, 
+    IonInputPasswordToggle, 
+    IonItem, 
     IonMenuButton, 
     IonPage, 
+    IonRow, 
+    IonTextarea, 
     IonTitle, 
+    IonToast, 
     IonToolbar 
 } from '@ionic/react';
 
 const About: React.FC = () => {
+      
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot='start'>
-            <IonMenuButton></IonMenuButton>
-          </IonButtons>
-          <IonTitle>About</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        
-      </IonContent>
-    </IonPage>
+          <IonContent fullscreen>
+            <IonGrid className='avatar-flex'>
+            <IonRow>
+              <IonCol></IonCol>
+              <IonCol>
+                <h1>About</h1>
+                <IonAvatar className='login-avatar'>
+                  <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg"/>
+                </IonAvatar>
+              </IonCol>
+              <IonCol></IonCol>
+            </IonRow>
+            </IonGrid>
+            <div  className='login-content'>
+    
+              <IonButton
+                id="combined-button"
+                
+                expand="full"
+                shape="round"
+                fill="outline"
+              >
+               Signup
+              </IonButton>
+            </div>
+            
+            
+    
+          </IonContent>
+        </IonPage>
   );
 };
 
