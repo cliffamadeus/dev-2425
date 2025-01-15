06e605a27929d538466077969dd01986547a4ead
cliffamadeus <dev.clfm5t12@gmail.com>
2025-01-15 10:11:10 +0800

    Added changes on about.tsx

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -6,6 +6,8 @@ import {
     IonContent, 
     IonGrid, 
     IonHeader, 
+    IonItem, 
+    IonLabel, 
     IonMenuButton, 
     IonPage, 
     IonRow, 
@@ -31,8 +33,30 @@ const About: React.FC = () => {
           <div className='about-flex'>
             <IonAvatar className='about-avatar'>
                     <img alt="Silhouette of a person's head" src="https://raw.githubusercontent.com/cliffamadeus/it35-app/refs/heads/dev-profile/src/pages/About/avatar.png"/>
-            </IonAvatar>        
+            </IonAvatar>
+
+            <IonItem> 
+              <IonLabel>
+                <h1>CLIFFMEISTER</h1>
+                <p>Programmer</p>
+              </IonLabel>
+            </IonItem>
+                
           </div>
+          
+          <IonItem> 
+              <IonLabel>
+                <h1>About</h1>
+                <p></p>
+              </IonLabel>
+          </IonItem>
+
+          <IonItem> 
+              <IonLabel>
+                <p>This application is built by the greatest slacker that's ever lived</p>
+              </IonLabel>
+          </IonItem>
+
           </IonContent>
         </IonPage>
   );
