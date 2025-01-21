34ae9b212dcb12ff0eb8bacc15e149ec0a79db18
cliffamadeus <dev.clfm5t12@gmail.com>
2025-01-21 22:28:18 +0800

    Updates

--- assets/ios.transition-legacy-9HyR3Sqz.js
+++ assets/ios.transition-legacy-9HyR3Sqz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DnBfwjNx.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DPpiZ80R.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
