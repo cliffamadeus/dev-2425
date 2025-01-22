6371197b44c78811f750fb9c61dde25bef1944d6
cliffamadeus <dev.clfm5t12@gmail.com>
2025-01-22 20:55:04 +0800

    Standby loader changes

--- src/pages/Home/Tabs/FeedPage.tsx
+++ src/pages/Home/Tabs/FeedPage.tsx
@@ -17,6 +17,7 @@ import {
   IonRefresher,
   IonRefresherContent,
   RefresherEventDetail,
+  IonLoading,
 } from '@ionic/react';
 
 import './FeedPage.css';
@@ -29,7 +30,7 @@ interface PhotoItem {
 
 const FeedPage: React.FC = () => {
   const [items, setItems] = useState<PhotoItem[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);  // New state to track loading
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const generateRandomIndex = () => Math.floor(Math.random() * 1000);
 
@@ -41,7 +42,7 @@ const FeedPage: React.FC = () => {
       const data = await response.json();
       photos.push({
         title: `Photo by ${data.author}`,
-        description: data.download_url, // Using the URL as a placeholder description
+        description: data.download_url,
         index,
       });
     }
@@ -49,10 +50,10 @@ const FeedPage: React.FC = () => {
   };
 
   const loadInitialItems = async () => {
-    setIsLoading(true); // Set loading state to true
+    setIsLoading(true);
     const photoItems = await fetchPhotoDetails(10);
     setItems(photoItems);
-    setIsLoading(false); // Set loading state to false once data is loaded
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -93,10 +94,14 @@ const FeedPage: React.FC = () => {
           <IonRefresherContent></IonRefresherContent>
         </IonRefresher>
 
-        {/* Display loading message if items are being fetched */}
-        {isLoading ? (
-          <h1 className='ion-text-center' style={{ marginTop: '50%' }}>Loading feed entries... <br></br> Please standby</h1>
-        ) : (
+        {/* Display loading spinner if items are being fetched */}
+        <IonLoading
+          isOpen={isLoading}
+          message="Loading feed entries... Please standby"
+          spinner="circles"
+        />
+
+        {!isLoading && (
           <IonList>
             {items.map((item) => (
               <IonCard key={item.index}>
