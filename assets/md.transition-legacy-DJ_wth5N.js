52a0b6dd0b79fdb17fd4e1cf936a8ae30c55c557
cliffamadeus <dev.clfm5t12@gmail.com>
2025-01-22 20:55:59 +0800

    Updates

--- assets/md.transition-legacy-DJ_wth5N.js
+++ assets/md.transition-legacy-DJ_wth5N.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DItYxtVg.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CkAuLsqK.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
