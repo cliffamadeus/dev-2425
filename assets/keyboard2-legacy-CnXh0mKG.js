b2a465e731bd886cf8ca043942a52eae6838d611
cliffamadeus <dev.clfm5t12@gmail.com>
2025-01-24 20:57:14 +0800

    Updates

--- assets/keyboard2-legacy-CnXh0mKG.js
+++ assets/keyboard2-legacy-CnXh0mKG.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-KJT2JD_Y.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-DHBxgXTG.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
