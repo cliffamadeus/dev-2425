fdd1e59f498e899673aaffb8a4a4d22ec45de6ef
cliffamadeus <dev.clfm5t12@gmail.com>
2025-01-24 21:58:19 +0800

    Updates

--- assets/swipe-back-legacy-DrnTyQ6A.js
+++ assets/swipe-back-legacy-DrnTyQ6A.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DHBxgXTG.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BSPKHSxk.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
