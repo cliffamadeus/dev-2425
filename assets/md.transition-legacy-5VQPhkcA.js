d4f008636648eb86473765ca0af12986e64b7bf1
cliffamadeus <dev.clfm5t12@gmail.com>
2025-01-24 20:06:03 +0800

    Updates

--- assets/md.transition-legacy-5VQPhkcA.js
+++ assets/md.transition-legacy-5VQPhkcA.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CWOoXHFw.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CYBVXmQ0.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
